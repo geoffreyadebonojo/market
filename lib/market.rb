b31a7b529cd7f1f45d9e1824af3e9b4d783fb18c
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-12 13:34:50 -0600

    attr reader test

--- lib/market.rb
+++ lib/market.rb
@@ -1,5 +1,7 @@
 class Market
 
+  attr_reader :name
+
   def initialize(name)
     @name = name
   end
--- test/market_test.rb
+++ test/market_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
+require './lib/vendor'
 require './lib/market'
 require 'pry'
 
@@ -12,6 +13,10 @@ class MarketTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Market, @m
+  end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @m.name
   end
 
 end
